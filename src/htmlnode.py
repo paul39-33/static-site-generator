3195d2dc16922976916a3c1b01b86ca4023003dd
paulus <paulusojak@gmail.com>
2025-05-07 15:48:08 +0700

    Add LeafNode for child note and its tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,26 @@ class HTMLNode:
         self.value == target.value and 
         self.children == target.children and 
         self.props == target.props)
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, [], props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return str(self.value)
+
+        props_html = ""
+
+        if self.props:
+            props_html = ' ' + self.props_to_html()
+        
+        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+        
+
+            
+        
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -17,6 +17,27 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "This is a test", props={'a': 'aaa', 'b': 'bbb'})
         node2 = HTMLNode("p", "This is a test", props={'c': 'ccc', 'd': 'ddd'})
         self.assertNotEqual(node, node2)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a_with_multiple_attributes(self):
+        # Create a link with href, class, and id attributes
+        node = LeafNode(
+            "a", 
+            "Visit Boot.dev", 
+            {
+                "href": "https://boot.dev",
+                "class": "external-link primary-btn",
+                "id": "learn-coding-link",
+                "target": "_blank"
+            }
+        )
+        
+        # Check the rendered HTML
+        expected_html = '<a href="https://boot.dev" class="external-link primary-btn" id="learn-coding-link" target="_blank">Visit Boot.dev</a>'
+        self.assertEqual(node.to_html(), expected_html)
 
 if __name__ == "__main__":
     unittest.main()
